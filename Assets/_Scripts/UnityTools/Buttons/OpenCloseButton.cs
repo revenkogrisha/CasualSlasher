e55fd82fc3c535ba0fb7353ff159da8fd4e0fa74
Grisha Revenko <grisha@revenko.org>
2022-09-01 19:32:40 +0300

    Added UnityTools

---
+++ Assets/_Scripts/UnityTools/Buttons/OpenCloseButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace TwoDoors.GUI.Buttons
+{
+    /// <summary>
+    /// Activates and disactivates GameObjects.
+    /// It's also able to add reference only to one object. (Only open or only close)
+    /// </summary>
+    public class OpenCloseButton : UIButton
+    {
+        [SerializeField] private GameObject _toOpen;
+        [SerializeField] private GameObject _toClose;
+
+        protected override void OnClicked()
+        {
+            if (_toOpen != null)
+                _toOpen.SetActive(true);
+
+            if (_toClose != null)
+                _toClose.SetActive(false);
+        }
+    }
+}
