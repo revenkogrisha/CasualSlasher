8e88b4c32ab0b57ab04693c62a40713306ebaf33
revenkogrisha <grisha@revenko.org>
2022-09-14 21:41:53 +0300

    Removed jump

--- Assets/_Scripts/Player/CharacterAnimator.cs
+++ Assets/_Scripts/Player/CharacterAnimator.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
 [RequireComponent(typeof(RelativeMovement))]
-[RequireComponent(typeof(RelativeJump))]
 public class CharacterAnimator : MonoBehaviour
 {
     public const string Running = nameof(Running);
@@ -10,26 +9,20 @@ public class CharacterAnimator : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     private RelativeMovement _relativeMovement;
-    private RelativeJump _relativeJump;
 
     private void Awake()
     {
-        _relativeJump = GetComponent<RelativeJump>();
         _relativeMovement = GetComponent<RelativeMovement>();
     }
 
     private void OnEnable()
     {
-        _relativeJump.OnJumpStarted += EnableJumping;
-        _relativeJump.OnJumpEnded += DisableJumping;
         _relativeMovement.OnRunStarted += EnableRunning;
         _relativeMovement.OnRunEnded += DisableRunning;
     }
 
     private void OnDisable()
     {
-        _relativeJump.OnJumpStarted -= EnableJumping;
-        _relativeJump.OnJumpEnded -= DisableJumping;
         _relativeMovement.OnRunStarted -= EnableRunning;
         _relativeMovement.OnRunEnded -= DisableRunning;
     }
@@ -42,15 +35,5 @@ public class CharacterAnimator : MonoBehaviour
     private void DisableRunning()
     {
         _animator.SetBool(Running, false);
-    }
-    
-    private void EnableJumping()
-    {
-        _animator.SetBool(Jumped, true);
-    }
-
-    private void DisableJumping()
-    {
-        _animator.SetBool(Jumped, false);
     }
 }
