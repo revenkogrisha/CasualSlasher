6f533f3317ddab3e6090e28b3b385d65f6ec15c4
revenkogrisha <grisha@revenko.org>
2022-11-26 22:05:25 +0300

    Refactored: removed color dispaly

--- Assets/Scripts/Characters/CharacterDamageDisplay.cs
+++ Assets/Scripts/Characters/CharacterDamageDisplay.cs
@@ -7,11 +7,6 @@ public class CharacterDamageDisplay : MonoBehaviour
 {
     [SerializeField] private Character _character;
 
-    [Header("Color")]
-    [SerializeField] private bool _displayColor = false;
-    [SerializeField] private MeshRenderer[] _renderers;
-    [SerializeField] private Color _damageColor;
-
     [Header("Scale")]
     [SerializeField] [Range(0f, 0.3f)] private float _scaleDuration;
     [SerializeField] [Range(0.5f, 1f)] private float _scaleAmount;
@@ -38,9 +33,6 @@ public class CharacterDamageDisplay : MonoBehaviour
     public void DisplayDamage()
     {
         DisplayScale();
-
-        if (_displayColor)
-            StartCoroutine(DisplayColor());
     }
 
     private void DisplayScale()
@@ -48,29 +40,6 @@ public class CharacterDamageDisplay : MonoBehaviour
         DOTween.Sequence()
             .Append(transform.DOScale(.8f, _scaleDuration))
             .Append(transform.DOScale(1f, _scaleDuration));
-    }
-
-    private IEnumerator DisplayColor()
-    {
-        var originalColors = GetOriginalColors(_renderers);
-
-        foreach (var renderer in _renderers)
-            renderer.material.color = _damageColor;
-
-        yield return new WaitForSeconds(_displayDurationInSeconds);
-
-        for (var i = 0; i < _renderers.Length; i++)
-            _renderers[i].material.color = originalColors[i];
-    }
-
-    private Color[] GetOriginalColors(MeshRenderer[] renderers)
-    {
-        var originalColors = new List<Color>();
-
-        for (var i = 0; i < renderers.Length; i++)
-            originalColors.Add(renderers[i].material.color);
-
-        return originalColors.ToArray();
     }
 
     private void DisplayDeath()
