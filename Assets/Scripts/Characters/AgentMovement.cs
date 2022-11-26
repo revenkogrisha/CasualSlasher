36b8b3e82e2a16fa760058a16bd2a3cd265ac940
revenkogrisha <grisha@revenko.org>
2022-11-26 19:59:48 +0300

    Added movement block on distance in front of target

--- Assets/Scripts/Characters/AgentMovement.cs
+++ Assets/Scripts/Characters/AgentMovement.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AgentMovement : MonoBehaviour
 {
     [SerializeField] private Character _character;
+    [SerializeField] private Transform _hitSphere;
+    [SerializeField] private float _hitRadius;
+    [SerializeField] private float _stopDistance = 3f;
     [SerializeField] private NavMeshAgent _navMeshAgent;
 
     private Transform _target;
+    private LayerMask _targetLayer;
+    private bool _isMovementBlocked = false;
 
     public event Action OnMovementStarted;
     public event Action OnMovementStopped;
@@ -25,16 +31,36 @@ public class AgentMovement : MonoBehaviour
         TrySetDestination();
         TryInvokeMovementEvents();
     }
+
     #endregion
 
     public void SetTarget(Transform target) => _target = target;
 
-    public void SetSpeed() => _navMeshAgent.speed = _character.Stats.MovementSpeed;
+    public void ApplyTargetLayer() => _targetLayer = _target.gameObject.layer;
+
+    public void ApplySpeed() => _navMeshAgent.speed = _character.Stats.MovementSpeed;
 
     private void TrySetDestination()
     {
-        if (_target && _navMeshAgent.isOnNavMesh)
-            _navMeshAgent.SetDestination(_target.position);
+        if (Physics.Raycast(transform.position, transform.forward, out var hit))
+            if (!_isMovementBlocked && hit.collider.gameObject.layer == _targetLayer && hit.distance < _stopDistance)
+                StartCoroutine(BlockMovement());
+
+        if (!_target)
+            return;
+
+        _navMeshAgent.SetDestination(_target.position);
+    }
+
+    private IEnumerator BlockMovement()
+    {
+        _navMeshAgent.speed = 0f;
+        _isMovementBlocked = true;
+
+        yield return new WaitForSeconds(1f);
+
+        ApplySpeed();
+        _isMovementBlocked = false;
     }
 
     private void TryInvokeMovementEvents()
