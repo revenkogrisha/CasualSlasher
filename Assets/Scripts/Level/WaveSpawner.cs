5937a2d40e6035a07e84e9499d7e766ee5a044b9
revenkogrisha <grisha@revenko.org>
2022-12-04 00:38:00 +0300

    Refactored character factory

--- Assets/Scripts/Level/WaveSpawner.cs
+++ Assets/Scripts/Level/WaveSpawner.cs
@@ -5,8 +5,12 @@ using UnityTools;
 public class WaveSpawner : MonoBehaviour
 {
     [Header("Components")]
-    [SerializeField] private CharacterFactory _factory;
     [SerializeField] private GroundCheck _groundCheck;
+
+    [Header("Characters")]
+    [Tooltip("Set if factory uses NavMesh or demands it")]
+    [SerializeField] private AgentTarget _target;
+    [SerializeField] private StatsConfig _statsConfig;
 
     [SerializeField] private Character _characterPrefab;
     [SerializeField] private Transform _originTransform;
@@ -15,12 +19,14 @@ public class WaveSpawner : MonoBehaviour
     [SerializeField] [Range(0f, 20f)] private float _spawnRadius = 15f;
     [SerializeField] [Range(0, 4)] private int _countInGroup = 3;
 
+    private ICharacterFactory _factory;
     private Timer _timer;
 
     #region MonoBehaviour
 
     private void Awake()
     {
+        _factory = new AgentCharacterFactory(_target);
         _timer = GetComponent<Timer>();
     }
 
@@ -47,7 +53,8 @@ public class WaveSpawner : MonoBehaviour
 
             var character = Instantiate(_characterPrefab, spawnPosition, Quaternion.identity);
 
-            _factory.InitCharacter(character);
+            character = _factory.InitStats(character, _statsConfig);
+            character = TrySetupMovement(character, _factory);
 
             i++;
         }
@@ -62,5 +69,14 @@ public class WaveSpawner : MonoBehaviour
             center.x + offset.x,
             center.y,
             center.z + offset.y);
+    }
+
+    private Character TrySetupMovement(Character character, ICharacterFactory factory)
+    {
+        if (factory is not IMoveableCharacterFactory moveableFactory)
+            return character;
+
+        character = moveableFactory.SetupMovement(character);
+        return character;
     }
 }
