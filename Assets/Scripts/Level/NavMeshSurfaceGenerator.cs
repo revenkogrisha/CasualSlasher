5d2e63f2dcc1eeb7e8d7762ee2d673458e201df3
revenkogrisha <grisha@revenko.org>
2022-12-03 23:58:28 +0300

    Refactored removed inheritance from MonoBehaviour

--- Assets/Scripts/Level/NavMeshSurfaceGenerator.cs
+++ Assets/Scripts/Level/NavMeshSurfaceGenerator.cs
@@ -3,13 +3,5 @@ using UnityEngine.AI;
 
 public class NavMeshSurfaceGenerator : MonoBehaviour
 {
-    [SerializeField] private NavMeshSurface _navMeshSurface;
-
-    #region MonoBehaviour
-
-    private void Start() => GenerateNavMeshSurface(_navMeshSurface);
-
-    #endregion
-
-    private void GenerateNavMeshSurface(NavMeshSurface surface) => surface.BuildNavMesh();
+    public void GenerateNavMeshSurface(NavMeshSurface surface) => surface.BuildNavMesh();
 }
