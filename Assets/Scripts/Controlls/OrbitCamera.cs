40a0e18f6b36133d93835778a785def3c5cf41cb
revenkogrisha <grisha@revenko.org>
2022-12-08 18:00:19 +0300

    Reimplemented OribitCamera without MonoBehaviour

--- Assets/Scripts/Controlls/OrbitCamera.cs
+++ Assets/Scripts/Controlls/OrbitCamera.cs
@@ -2,28 +2,30 @@ using UnityEngine;
 
 public class OrbitCamera : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private Vector3 _cameraOffset = new(0f, 4f, -16f);
-
+    private Transform _target;
     private Transform _transform;
-    private Vector3 _offset;
-
-    #region MonoBehaviour
-
-    private void LateUpdate() => TryApplyRotation();
-
-    #endregion
+    private Vector3 _cameraOffset = new(0f, 4f, -10f);
+    private Vector3 _distanceToTarget;
 
     public void SetTarget(Transform target) => _target = target;
 
-    public void Init(Vector3 playerPosition)
+    public void Init(Transform transform, Vector3 playerPosition)
     {
         _transform = transform;
         if (!_target)
             throw new System.Exception("Target field is unassigned!");
 
         SetPosition(playerPosition);
-        _offset = _target.position - _transform.position;
+        _distanceToTarget = _target.position - _transform.position;
+    }
+
+    public void TryApplyCameraTransform()
+    {
+        if (!_target)
+            return;
+
+        _transform.position = _target.position - _distanceToTarget;
+        _transform.LookAt(_target);
     }
 
     private void SetPosition(Vector3 playerPosition)
@@ -34,14 +36,5 @@ public class OrbitCamera : MonoBehaviour
             playerPosition.z + _cameraOffset.z);
         
         _transform.position = cameraPosition;
-    }
-
-    private void TryApplyRotation()
-    {
-        if (!_target)
-            return;
-
-        _transform.position = _target.position - _offset;
-        _transform.LookAt(_target);
     }
 }
