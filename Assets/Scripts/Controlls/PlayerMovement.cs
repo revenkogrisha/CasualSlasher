e1294242e86dfffe2e522448b37c8ffe9cd935a8
revenkogrisha <grisha@revenko.org>
2022-12-17 22:34:08 +0300

    Reimplemented & renamed PlayerMovement

--- Assets/Scripts/Controlls/PlayerMovement.cs
+++ Assets/Scripts/Controlls/PlayerMovement.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 namespace SaveTheGuy.Control
 {
-    public class RelativeMovement : MonoBehaviour, IMoveable
+    public class PlayerMovement : MonoBehaviour, IMoveable
     {
         [Header("Components")]
         [SerializeField] private CharacterController _characterController;
         [SerializeField] private Animator _animator;
 
         [Header("Settings")]
-        [SerializeField] private float _speed = 5f;
-        [SerializeField] private float _rotationSpeed = 10f;
+        [SerializeField] [Range(0f, 5f)] private float _horizontalSpeed = 5f;
+        [SerializeField] [Range(0f, 10f)] private float _forwardSpeed = 5f;
+        [Tooltip("Needed for clamping horizontal & forward speed")]
+        [SerializeField] [Range(0f, 10f)] private float _maxSpeed = 5f;
 
         private Transform _transform;
         private MovementAnimator _characterAnimator;
+        private PlayerInput _input;
 
         public event Action OnRunStarted;
         public event Action OnRunEnded;
@@ -25,6 +28,7 @@ namespace SaveTheGuy.Control
         private void Awake()
         {
             _characterAnimator = new(_animator);
+            _input = new();
             _transform = transform;
         }
 
@@ -40,31 +44,37 @@ namespace SaveTheGuy.Control
             OnRunEnded -= _characterAnimator.DisableRunning;
         }
 
+        private void Update()
+        {
+            var movement = GetVectorWithSpeed(_input);
+            TryMove(movement);
+        }
+
         #endregion
 
         public void TryMove(Vector3 movement)
         {
-            movement = UpdateSpeed(movement, _speed);
-            if (movement.x == 0 && movement.z == 0)
+            if (CheckIsStationary(movement))
             {
                 OnRunEnded?.Invoke();
                 return;
             }
 
             OnRunStarted?.Invoke();
-            
-            ApplyBodyDirection(movement);
 
             movement = ApplyGravity(movement);
             Move(movement);
         }
 
-        private Vector3 UpdateSpeed(Vector3 movement, float speed)
+        private Vector3 GetVectorWithSpeed(PlayerInput input)
         {
-            movement *= speed;
-            movement = Vector3.ClampMagnitude(movement, speed);
+            var movement = input.GetMovementVector(_forwardSpeed, _horizontalSpeed);
+            movement = Vector3.ClampMagnitude(movement, _maxSpeed);
             return movement;
         }
+
+        private bool CheckIsStationary(Vector3 movement)
+            => movement.x == 0 && movement.z == 0;
 
         private void ApplyBodyDirection(Vector3 movement)
         {
@@ -76,7 +86,7 @@ namespace SaveTheGuy.Control
             _transform.rotation = Quaternion.Lerp(
                 _transform.rotation,
                 direction,
-                _rotationSpeed * Time.deltaTime);
+                _forwardSpeed * Time.deltaTime);
         }
 
         private Vector3 ApplyGravity(Vector3 movement)
