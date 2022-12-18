f0eb87e35e85bb68af791a665c64c8e064c3c10e
revenkogrisha <grisha@revenko.org>
2022-12-18 18:01:53 +0300

    Changed horizontal axis to joystick.Horizontal

--- Assets/Scripts/Controlls/PlayerInput.cs
+++ Assets/Scripts/Controlls/PlayerInput.cs
@@ -4,15 +4,15 @@ namespace SaveTheGuy.Control
 {
     public class PlayerInput
     {
-        public Vector3 GetMovementVector()
+        public Vector3 GetMovementVector(Joystick joystick)
         {
-            var horizontal = Input.GetAxis(Axis.MouseX);
+            var horizontal = joystick.Horizontal;
             return new(horizontal, 0f, 1f);
         }
 
-        public Vector3 GetMovementVector(float forwardSpeed, float horizontalSpeed)
+        public Vector3 GetMovementVector(Joystick joystick, float forwardSpeed, float horizontalSpeed)
         {
-            var horizontal = Input.GetAxis(Axis.MouseX);
+            var horizontal = joystick.Horizontal;
             return new(
                 horizontal * horizontalSpeed,
                 0f,
