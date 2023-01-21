0e29376424c378057dd48c6577bd4ab4a01f28ec
Revenko Grisha <grisha@revenko.org>
2023-01-22 00:45:21 +0300

    Added Color Bubbles

---
+++ Assets/Scripts/ColorFeatures/ColorBubble.cs
@@ -0,0 +1,23 @@
+using ColorManRun.Characters;
+using ColorManRun.ColorFeatures;
+using UnityEngine;
+using UnityTools;
+
+namespace ColorManRun.ColorFeatures
+{
+    public class ColorBubble : MonoBehaviour
+    {
+        [SerializeField] private GameColor _color;
+        [SerializeField] private Material _material;
+
+        public GameColor Color => _color;
+        public Material Material => _material;
+
+        public void Burst()
+        {
+            Destroy(gameObject);
+
+            // Some particle effect or dotween animation
+        }
+    }
+}
