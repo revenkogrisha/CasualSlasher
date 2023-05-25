010b62d5d00582cb2cb5bdc1688d083f7f41d800
Grisha Revenko <grisha@revenko.org>
2023-05-25 11:16:32 +0300

    Removed unnecesarry code

--- Assets/Scripts/Generators & Spawners/GameStart.cs
+++ Assets/Scripts/Generators & Spawners/GameStart.cs
@@ -8,27 +8,21 @@ namespace CasualSlasher.Generators
     public class GameStart : MonoBehaviour
     {
         [Header("Components")]
-        [SerializeField] private PlatformGenerator _platformGenerator;
         [SerializeField] private CharacterSpawner _characterSpawner;
-
-        [Header("Player")]
-        [SerializeField] private Joystick _joystick;
-        [SerializeField] private OrbitCamera _orbitCamera;
-
-        [Header("Prefabs")]
-        [SerializeField] private PlayerCharacter _playerPrefab;
 
         [Header("Level")]
         [SerializeField] private NavMeshSurface _navSurface;
-        [SerializeField] private Vector3 _playerSpawnPosition = Vector3.forward;
-        
-        private LevelGenerator _levelGenerator;
 
-        private void GenerateLevel(LevelGenerator generator)
+        private INavMeshSurfaceGenerator _navSurfaceGenerator;
+
+        private void Awake()
         {
-            generator.GenerateLevel(
-                _navSurface,
-                _playerSpawnPosition);
+            _navSurfaceGenerator = new NavMeshSurfaceGenerator();
+        }
+
+        private void Start()
+        {
+            _navSurfaceGenerator.GenerateSurface(_navSurface);
         }
     }
 }
--- Assets/Scripts/Generators & Spawners/LevelGenerator.cs
+++ Assets/Scripts/Generators & Spawners/LevelGenerator.cs
@@ -8,7 +8,6 @@ namespace CasualSlasher.Generators
 {
     public class LevelGenerator
     {
-        private ISurfaceGenerator _surfaceGenerator;
         private INavMeshSurfaceGenerator _navGenerator;
         private CharacterSpawner _characterSpawner;
         private PlayerCharacter _playerPrefab;
@@ -16,11 +15,9 @@ namespace CasualSlasher.Generators
         public event Action<PlayerCharacter> OnPlayerSpawned;
 
         public LevelGenerator(
-            ISurfaceGenerator surfaceGenerator, 
             CharacterSpawner characterSpawner, 
-            PlayerCharacter playerPrefab)
+            PlayerCharacter playerPrefab) // ***
         {
-            _surfaceGenerator = surfaceGenerator;
             _navGenerator = new NavMeshSurfaceGenerator();
             _characterSpawner = characterSpawner;
             _playerPrefab = playerPrefab;
@@ -30,22 +27,7 @@ namespace CasualSlasher.Generators
             NavMeshSurface navSurface,
             Vector3 playerSpawnPosition)
         {
-            GenerateSuface(navSurface);
 
-            SpawnPlayer(playerSpawnPosition);
-        }
-
-        private Finish GenerateSuface(NavMeshSurface surface)
-        {
-            var finish = _surfaceGenerator.GenerateSurface();
-            _navGenerator.GenerateSurface(surface);
-            return finish;
-        }
-
-        private void SpawnPlayer(Vector3 playerSpawnPosition)
-        {
-            var player = _characterSpawner.Spawn(_playerPrefab, playerSpawnPosition);
-            OnPlayerSpawned?.Invoke(player);
         }
     }
 }
